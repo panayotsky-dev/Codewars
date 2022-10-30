ab09b6387bfc7be98d477c7f15f4dc001f4e56ae
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-10-31 01:15:15 +0200

    Add files via upload

---
+++ JavaScript/shortestWord.js
@@ -0,0 +1,17 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+
+
+function findShort(s){
+    let arr = s.split(' ');
+    let shortest = arr[0].length;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i].length < shortest) {
+        shortest = arr[i].length;
+        }
+    }
+    return shortest;
+    }
+
