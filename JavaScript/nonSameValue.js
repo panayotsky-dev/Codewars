1d99499f01577b53ab2de3b4046210cb20ca136d
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-10-30 03:18:36 +0300

    Add files via upload

---
+++ JavaScript/nonSameValue.js
@@ -0,0 +1,21 @@
+// Implement the function unique_in_order which takes as argument a sequence and
+//  returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+function uniqueInOrder(iterable){
+  let array = [];
+  for(let i = 0; i < iterable.length; i++){
+    if(iterable[i] !== iterable[i + 1]){
+      array.push(iterable[i])
+    }
+  }
+  return array;
+}
+
+
+//uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+
+//function(iterable){
+//     return [...iterable].filter((a, i) => a !== iterable[i-1])
+// }
